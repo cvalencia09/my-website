c22a7061fb89dfe2d25dd44c505ab01e32a30527
cvalencia09 <cristian.valencia09@gmail.com>
2024-12-18 17:11:02 -0600

    asas

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,5 @@
 
-import __ASTRO_IMAGE_IMPORT_Z29FMdQ from "./random/DSCF8244.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Falbums%2Frandom.yml";
 import __ASTRO_IMAGE_IMPORT_ZR2Dxd from "./2023-mexico/_SDI1122.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Falbums%2F2023-mexico.yml";
-export default new Map([["./random/DSCF8244.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Falbums%2Frandom.yml", __ASTRO_IMAGE_IMPORT_Z29FMdQ], ["./2023-mexico/_SDI1122.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Falbums%2F2023-mexico.yml", __ASTRO_IMAGE_IMPORT_ZR2Dxd]]);
+import __ASTRO_IMAGE_IMPORT_Z29FMdQ from "./random/DSCF8244.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Falbums%2Frandom.yml";
+export default new Map([["./2023-mexico/_SDI1122.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Falbums%2F2023-mexico.yml", __ASTRO_IMAGE_IMPORT_ZR2Dxd], ["./random/DSCF8244.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Falbums%2Frandom.yml", __ASTRO_IMAGE_IMPORT_Z29FMdQ]]);
 		
